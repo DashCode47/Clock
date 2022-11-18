b12f587f91561d9853cb6c84dc72a8f1a2d3e6fb
David Lozada <davsh47@hotmail.com>
2022-11-19 01:25:12 +0300

    clock functional

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Clock = () => {
+  const [hours, sethours] = useState();
+  const [minutes, setminutes] = useState();
+  const [seconds, setseconds] = useState();
+
+  const handleDate = () => {
+    const date = new Date();
+    setseconds(date.getSeconds());
+    sethours(date.getHours());
+    setminutes(date.getMinutes());
+  };
+
+  useEffect(() => {
+    const setTimeOut = setInterval(() => {
+      handleDate();
+    }, 1000);
+
+    return () => clearInterval(setTimeOut);
+  }, []);
+
   return (
     <div className="clock">
-      <div className="hand hour" data-hour-hand></div>
-      <div className="hand minute" data-minute-hand></div>
-      <div className="hand second" data-second-hand></div>
+      <div
+        className="hand hour"
+        data-hour-hand
+        style={{ transform: `rotate(${hours * 30}deg)` }}
+      ></div>
+      <div
+        className="hand minute"
+        data-minute-hand
+        style={{ transform: `rotate(${minutes * 6}deg)` }}
+      ></div>
+      <div
+        className="hand second"
+        data-second-hand
+        style={{ transform: `rotate(${seconds * 6}deg)` }}
+      ></div>
       <div className="number number1">1</div>
       <div className="number number2">2</div>
       <div className="number number3">3</div>
